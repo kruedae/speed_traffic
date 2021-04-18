08a70c3434ff0b222f1337759a5530a73832791b
kruedae <kruedae@unal.edu.co>
2021-04-17 19:10:31 -0500

    Cambios
    -La clase Place ahora puede usar addPlace() con y sin parametros
    - El main tiene dos carriles de 5 places interceptados por el del medio.
    De momento estan hechos a mano.

--- Speed_Traffic/src/speed_traffick_pack/Main.java
+++ Speed_Traffic/src/speed_traffick_pack/Main.java
@@ -6,9 +6,22 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Road calle1 = new Road("North");
+		Place aux = null;
 		System.out.println(calle1.getEnd());
 		for(int i=0; i<5; i++) {
 			calle1.addPlace();
+			if (i==2) {
+				aux = calle1.getEnd();
+			}
+		}
+		Road calle2 = new Road("West");
+		for(int i=0; i<5; i++) {
+			calle2.addPlace();
+			if (i==2) {
+				calle2.addPlace(aux);
+				System.out.println(aux);
+				
+			}
 		}
 		System.out.println(calle1.getEnd());
 
