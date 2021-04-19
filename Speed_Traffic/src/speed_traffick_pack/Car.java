0363acee5da796edf5ecfdf081bd1169130e916a
juanfelipepast2 <jpastranar@unal.edu.co>
2021-04-19 14:48:23 -0500

    Subir clase car y car_queue

--- Speed_Traffic/src/speed_traffick_pack/Car.java
+++ Speed_Traffic/src/speed_traffick_pack/Car.java
@@ -1,8 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package speed_traffick_pack;
 
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
 public class Car {
-    Color color;
+    String color;
     Integer direction;
-    Place position; //provisional
+    Boolean place;
+    public Car(){
+    }
+
+    public Car(String color, Integer direction, Boolean place) {
+        this.color = color;
+        this.direction = direction;
+        this.place = place;
+    }
+
+    public Car(String color) {
+        this.color = color;
+    }
+    
+    
+    
     
 }
